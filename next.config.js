999213bfaf2d100f5e327d247be2984bb8d24e37
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-03-30 16:34:55 +0200

    latest upadates

--- next.config.js
+++ next.config.js
@@ -21,7 +21,11 @@ module.exports = nextTranslate({
   swcMinify: true,
   optimizeFonts: true,
    
-  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
